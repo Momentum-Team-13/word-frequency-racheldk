5baa145a866feb306f0ad11aa056ea69f69a8b22
Rachel Kelly <racheldk126@gmail.com>
2022-06-19 08:42:19 -0700

    working, but with line breaks still in

--- word_frequency.py
+++ word_frequency.py
@@ -11,22 +11,26 @@ STOP_WORDS = [
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     # ALL OF MY CODE GOES INSIDE THIS FUNCTION
-
     # in terminal I'll type: python word_frequency.py [file name]
+    
     # print(f'your file is {file}')
     with open(file) as open_file:
-        # we can either read or readlines 
         text = open_file.read()
+        print(text)
         
         no_punctuation = text.replace("?", "").replace(",", "").replace(".", "").replace("!", "").replace('"', '').replace(':', '').replace(';', '')
-        print(f'no punctuation: {no_punctuation}')       
+        # print(f'no punctuation: {no_punctuation}')       
   
         lower_case = no_punctuation.lower()
         # print(f'lower_case: {lower_case}')
+
         text_list = lower_case.split(" ")
-        # print(f'text_list: {text_list}')
+    #  FIND A WAY TO GET THE NEW LINES /N OUT
+        print(f'text_list: {text_list}')
+
         words_to_keep = []
         words_to_count = []
+
         def check_words(text_list):
             for word in text_list:
                 if word in STOP_WORDS:
@@ -37,32 +41,32 @@ def print_word_freq(file):
                     words_to_keep.append(word)
                     # print(f'Now the word count list is: {words_to_keep}')
         check_words(text_list)
-        print(words_to_keep)   
+        # print(words_to_keep)   
 
         def do_not_add_repeat_words(input):
             for word in input:
                 # if the word does not already appear in our final tally list, add it to the list. if not, don't do anything
                 if word in words_to_count:
                     word = f'{word} already in list'
-                    print(word)
+                    # print(word)
                 else: 
                     words_to_count.append(word)
-                    print(f'number of {word}: {words_to_keep.count(word)}')
+                    # print(f'number of {word}: {words_to_keep.count(word)}')
     
         do_not_add_repeat_words(words_to_keep)
-        print(f'words to count: {words_to_count}')
+        # print(f'words to count: {words_to_count}')
 
         word_totals = {}
     
         def compile_word_counts(input):
             for word in input:
                 word_totals[word] = words_to_keep.count(word)
-            print(word_totals)  
+            # print(word_totals)  
         
         compile_word_counts(words_to_count)
         # word_totals['star_test'] = '*' * 3
         # word_totals['star_test'] = 3
-        print(f'word total dictionary: {word_totals}')
+        # print(f'word total dictionary: {word_totals}')
         # print(word_totals['star_test'])
         # print('*' * int(word_totals['hello']))
 
@@ -72,7 +76,7 @@ def print_word_freq(file):
                 bar = "|"
                 number = word_totals[word]
                 star = '*' * int(word_totals[word])
-                print("{:<20} {:<1} {:<2} {:<10}".format(name.rjust(20), bar, number, star))
+                print("{:<20} {:<1} {:<2} {:<40}".format(name.rjust(20), bar, number, star))
                 
         add_stars_and_format(word_totals)
 
